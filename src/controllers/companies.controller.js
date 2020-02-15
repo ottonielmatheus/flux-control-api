5104b2f977fd1f6430987cc2f984363cfc20778f
Ottoniel Matheus <ottoniel.matheus@gmail.com>
2020-02-15 02:07:37 -0300

    tratamento de erros

--- src/controllers/companies.controller.js
+++ src/controllers/companies.controller.js
@@ -1,66 +1,69 @@
 const mysql = require('../mysql').pool;
 
 const get = (req, res, next) => {
-
-	try {
-        
-        mysql.getConnection((error, conn) => {
-
-            if (error) throw new Error(error);
     
+    mysql.getConnection((error, conn) => {
+
+        try {
+
+            if (error) throw error;
+
             conn.query(`SELECT * FROM companies WHERE id = ?`,
             [req.params.id],
             
             (error, results, field) => {
                 
-                conn.release();                
-                if (error) throw new Error(error);
-    
+                conn.release();
+                if (error) throw error;
+
                 return res.status(200).send({ 
                     company: results[0]
-                }); 
+                });
             });
-        });
-    }
+        }
 
-    catch (ex) {
-        return res.status(500).send({ error: ex });
-    }
+        catch (ex) {
+            res.status(500).send({ error: ex });
+        }
+        
+    });
 };
 
 const search = (req, res, next) => {
 
-    try {
+    mysql.getConnection((error, conn) => {
 
-        mysql.getConnection((error, conn) => {
+        try {
 
-            if (error) throw new Error(error);
-    
+            if (error) throw error;
+
             conn.query(`SELECT * FROM companies`,
             (error, results, field) => {
-    
+
+                if (error) throw error;
                 conn.release();
-                if (error) throw new Error(error); 
     
                 return res.status(200).send({ 
                     items: results 
                 });
-            });
-        });
-    }
 
-    catch (ex) {
-        return res.status(500).send({ error: ex });
-    }
+            });
+        }
+
+        catch (ex) {
+            res.status(500).send({ error: ex });
+        }
+
+    });
 };
 
 const add = (req, res, next) => {
 
-	try {
+    mysql.getConnection((error, conn) => {
 
-        mysql.getConnection((error, conn) => {
+        try {
 
-            if (error) throw new Error(error);
+            if (error) throw error;
 
             conn.query('INSERT INTO companies (name, thumbnail, created_at) VALUES (?, ?, ?)',
             [req.body.name, req.file.path, new Date()],
@@ -68,7 +71,7 @@ const add = (req, res, next) => {
             (error, results, field) => {
                 
                 conn.release();
-                if (error) throw new Error(error);
+                if (error) throw error;
 
                 return res.status(201).send({
                     message: "company_created",
@@ -79,70 +82,75 @@ const add = (req, res, next) => {
                     }
                 });
             });
-        });
-    }
+        }
 
-    catch (ex) {
-        return res.status(500).send({ error: ex });
-    }
+        catch (ex) {
+            return res.status(500).send({ error: ex });
+        }
+
+    });
 };
 
 const change = (req, res, next) => {
 
-	try {
+    mysql.getConnection((error, conn) => {
 
-        mysql.getConnection((error, conn) => {
+        try {
 
-            if (error) throw new Error(error);
-    
+            if (error) throw error;
+            
             conn.query(`UPDATE companies
                         SET name = ?, thumbnail = ?
                         WHERE id = ?`,
             [req.body.name, req.body.thumbnail, req.params.id],
             
             (error, results, field) => {
-    
+
                 conn.release();
-                if (error) throw new Error(error);
+                if (error) throw error;                
 
                 return res.status(202).send({
                     message: "company_updated"
                 });
             });
-        });
-    }
+        }
 
-    catch (ex) {
-        return res.status(500).send({ error: ex });
-    }
+        catch (ex) {
+            return res.status(500).send({ error: ex });
+        }
+
+    });
 };
 
 const remove = (req, res, next) => {
 
-    try {
-        
-        mysql.getConnection((error, conn) => {
-
-            if (error) throw new Error(error);
     
+        
+    mysql.getConnection((error, conn) => {
+
+        try {
+
+            if (error) throw error;
+
             conn.query(`DELETE FROM companies WHERE id = ?`,
             [req.params.id],
-    
+
             (error, results, field) => {
-    
+
                 conn.release();
-                if (error) throw new Error(error);
+                if (error) throw error;
 
                 return res.status(202).send({
                     message: "company_deleted" 
                 });
             });
-        });
-    }
+        }
 
-    catch (ex) {
-        return res.status(500).send({ error: ex });
-    }
+        catch (ex) {
+            return res.status(500).send({ error: ex });
+        }
+
+    });
 };
 
 exports.get = get;
