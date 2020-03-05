c96662ac95b9dea06358718eb508985a3d5d0e69
Ottoniel Matheus <ottoniel.matheus@gmail.com>
2020-03-04 21:33:34 -0300

    cors method allow updated

--- src/app.js
+++ src/app.js
@@ -13,10 +13,11 @@ app.use(bodyParser.json());
 
 // CORS
 app.use((req, res, next) => {
-    
+
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+
     next();
 });
 
